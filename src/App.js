69a727f1e3a94adfca5e91d05a845e93be2aa754
Unknown <ashleighrhazaly@gmail.com>
2018-08-20 21:20:52 +0800

    Import dashboard and remove starter kit code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import logo from "./logo.svg";
 import "./App.scss";
+import store from "./store";
+import Dashboard from "./components/dashboard/dashboard";
 
-const store = createStore(() => [], {}, applyMiddleware());
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            hello
-          </p>
-        </div>
+        <Dashboard />
       </Provider>
     );
   }
