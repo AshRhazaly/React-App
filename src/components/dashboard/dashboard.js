6c54e2b9aad6add6acedf0053495a3ce2e0b98af
Unknown <ashleighrhazaly@gmail.com>
2018-08-20 21:21:22 +0800

    Create dashboard component and header component

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from "./header/header";
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default Dashboard
