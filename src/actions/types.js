d02081bcd051f97af53be442af3051527960f9ad
Unknown <ashleighrhazaly@gmail.com>
2018-08-20 21:19:31 +0800

    Create action types for redux actions

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+// GET PORTFOLIO DATA
+export const GET_PORTFOLIO_DATA = "get_portfolio_data";
+export const GET_PORTFOLIO_DATA_SUCCESS = "get_portfolio_data_success";
+export const GET_PORTFOLIO_DATA_FAIL = "get_portfolio_data_fail";
