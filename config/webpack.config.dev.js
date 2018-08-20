84d3aeab28d2cfb5432234c1434ee2e72228934b
Unknown <ashleighrhazaly@gmail.com>
2018-08-20 20:14:49 +0800

    Configure SCSS

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -166,6 +166,10 @@ module.exports = {
                   importLoaders: 1,
                 },
               },
+
+              {
+                loader: require.resolve('sass-loader'),
+              },
               {
                 loader: require.resolve('postcss-loader'),
                 options: {
