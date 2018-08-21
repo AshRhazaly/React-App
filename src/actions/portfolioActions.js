d185964343f4076d07e0a7e78231e20292d28426
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 20:57:15 +0800

    Import portfolio api service class and integrate redux action

--- src/actions/portfolioActions.js
+++ src/actions/portfolioActions.js
@@ -2,23 +2,34 @@ import {
   GET_PORTFOLIO_DATA,
   GET_PORTFOLIO_DATA_FAIL,
   GET_PORTFOLIO_DATA_SUCCESS
-} from "../actions/types";
+} from "./types";
+import PortfolioApi from "./services/portfolio.api";
 
 export const getPortfolioDataSuccess = portfolioData => {
   return {
-    type: GET_PROFILE_SUCCESS,
+    type: GET_PORTFOLIO_DATA_SUCCESS,
     payload: portfolioData
   };
 };
 
 export const getPortfolioDataFail = error => {
   return {
-    type: GET_PROFILE_FAIL,
+    type: GET_PORTFOLIO_DATA_FAIL,
     payload: error
   };
 };
 export const getPortfolioData = () => {
   return dispatch => {
-    dispatch({ type: GET_PORTFOLIO_DATA })
-  }
-}
+    dispatch({ type: GET_PORTFOLIO_DATA });
+    return PortfolioApi.getData()
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        dispatch(getPortfolioDataSuccess(response));
+      })
+      .catch(error => {
+        dispatch(getPortfolioDataFail(error));
+      });
+  };
+};
