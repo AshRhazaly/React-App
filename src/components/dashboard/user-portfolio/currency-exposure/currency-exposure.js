09a75fd2143bbeddaa4f51e5631083ee38d83dae
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 22:47:07 +0800

    Iterate over the new currency exposure objects

--- src/components/dashboard/user-portfolio/currency-exposure/currency-exposure.js
+++ src/components/dashboard/user-portfolio/currency-exposure/currency-exposure.js
@@ -3,48 +3,23 @@ import "./currency-exposure.scss";
 import convertToPercentage from "../../../../helpers/TransformNumber";
 
 const CurrencyExposure = ({ currencyExposure }) => {
+  const exposureList = currencyExposure.map(data => {
+    return (
+      <li className="col-6 list-item" key={data.id}>
+        <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
+          <div className={`square ${data.label.toLowerCase()}`} />
+          <div className="col">{data.label}</div>
+          <span className="col">{convertToPercentage(data.value, 0)}</span>
+        </section>
+      </li>
+    );
+  });
 
   return (
     <section className="pt-5">
       <h5>Global Currency Exposure</h5>
       <div className="col-12 pt-3 currency-exposure">
-        <ul className="row list-unstyled">
-          <li className="col-6 list-item">
-            <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
-              <div className="square usd" />
-              <div className="col">USD</div>
-              <span className="col">{convertToPercentage(currencyExposure.USD, 0)}</span>
-            </section>
-          </li>
-          <li className="col-6 list-item">
-            <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
-              <div className="square jpy" />
-              <div className="col">JPY</div>
-              <span className="col">{convertToPercentage(currencyExposure.JPY, 0)}</span>
-            </section>
-          </li>
-          <li className="col-6 list-item">
-            <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
-              <div className="square sgd" />
-              <div className="col">SGD</div>
-              <span className="col">{convertToPercentage(currencyExposure.SGD, 0)}</span>
-            </section>
-          </li>
-          <li className="col-6 list-item">
-            <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
-              <div className="square others" />
-              <div className="col">Others</div>
-              <span className="col">{convertToPercentage(currencyExposure.Others, 0)}</span>
-            </section>
-          </li>
-          <li className="col-6 list-item">
-            <section className="shadow-sm p-3 mr-1 mb-3 bg-white rounded row">
-              <div className="square eur" />
-              <div className="col">EUR</div>
-              <span className="col">{convertToPercentage(currencyExposure.EUR, 0)}</span>
-            </section>
-          </li>
-        </ul>
+        <ul className="row list-unstyled">{exposureList}</ul>
       </div>
     </section>
   );
