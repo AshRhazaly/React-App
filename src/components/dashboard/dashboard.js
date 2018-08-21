fe26d9ca2f0597089d6fae496a5f06601fa306ad
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 19:48:56 +0800

    Import Overview and UserPortfolio components

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Header from "./header/header";
-import { Button } from "reactstrap";
+import Overview from "./overview/overview";
+import UserPortfolio from "./user-portfolio/user-portfolio";
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header />
+        <div className="p-5 container">
+          <Overview />
+          <UserPortfolio />
+        </div>
       </div>
     );
   }
