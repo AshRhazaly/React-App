348b62deb68ee91b0380d76b3c9ab4e9cd1a8765
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 20:59:32 +0800

    Pass data to currency exposure and asset allocation

--- src/components/dashboard/user-portfolio/user-portfolio.js
+++ src/components/dashboard/user-portfolio/user-portfolio.js
@@ -3,16 +3,15 @@ import "./user-portfolio.scss";
 import CurrencyExposure from "./currency-exposure/currency-exposure";
 import AssetAllocation from "./asset-allocation/asset-allocation";
 
-const UserPortfolio = () => {
+const UserPortfolio = ({ currencyExposureData, assetAllocationData }) => {
   return (
     <section className="user-portfolio-card card p-5 shadow p-3 mb-5 bg-white rounded">
       <h5 className="portfolio-header card-title text-center">
         Portfolio Composition
       </h5>
-
       <section className="card-body pt-5">
-        <CurrencyExposure />
-        <AssetAllocation />
+        <CurrencyExposure currencyExposure={currencyExposureData}/>
+        <AssetAllocation assetAllocation={assetAllocationData}/>
       </section>
     </section>
   );
