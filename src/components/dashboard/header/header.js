62d4b59ac5c106bc54b8c233833e76fb68bccfdb
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 19:49:10 +0800

    Create and style header component

--- src/components/dashboard/header/header.js
+++ src/components/dashboard/header/header.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { Button } from "reactstrap";
+import "./header.scss";
 
 const Header = () => {
   return (
-    <div className="container">
-      <Button color="danger">Danger!</Button>
-      <h1>I'm a header</h1>
-    </div>
+    <header className="headerContent">
+      <section className="container">
+        <section className="row">
+          <div className="col">
+            <p className="brandName">StashAway</p>
+          </div>
+          <div className="col">
+            <span className="row float-right">
+              <p className="px-4">Support</p>
+              <p className="px-4">Log Out</p>
+            </span>
+          </div>
+        </section>
+      </section>
+      <section className="headerOverview d-flex flex-column">
+        <section className="align-items-end">
+          <p className="previewText text-center">Preview your Portfolio</p>
+          <p className="changeText text-center headerTitle">You can always change this later.</p>
+        </section>
+      </section>
+    </header>
   );
 };
 
