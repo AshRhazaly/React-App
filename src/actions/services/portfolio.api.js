1297960b07d7df890badaf4fd75f3a591cc64ec3
Unknown <ashleighrhazaly@gmail.com>
2018-08-21 19:59:43 +0800

    Create static method to fetch data from endpoint

--- src/actions/services/portfolio.api.js
+++ src/actions/services/portfolio.api.js
@@ -1,2 +1,16 @@
 import { BASE_URL } from "./endpoint";
 
+export default class PortfolioApi {
+  static getData() {
+    return fetch(`${BASE_URL}userPortfolio`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json"
+      }
+    })
+    .then(response => {
+      return response;
+    })
+  }
+}
